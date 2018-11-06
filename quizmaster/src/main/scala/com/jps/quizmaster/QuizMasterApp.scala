73d0f3e461c3c711a7a8723e688f80d8247b439d
Unknown <nick.stumpos@gmail.com>
2018-11-05 21:51:41 -0600

    Im an idiot not binding to "localhost" anymore NPS

--- quizmaster/src/main/scala/com/jps/quizmaster/QuizMasterApp.scala
+++ quizmaster/src/main/scala/com/jps/quizmaster/QuizMasterApp.scala
@@ -32,7 +32,7 @@ object QuizMasterApp extends App {
         }
       }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
 
   implicit val t = Timeout(10 second)
